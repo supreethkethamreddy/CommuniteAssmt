231466dce4f61a8102bf4ce34290ea4bd69b84d0
supreethkethamreddy <supreethkethamreddy@gmail.com>
2025-04-18 19:38:05 +0530

    final change

--- client/src/src/components/Navbar/Navbar.jsx
+++ client/src/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
 import './Navbar.css'
 
+
+
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <>
         <nav className="navbar">
-            <div><link href='./cms'></link>About Us</div>
+            <div>About Us</div>
             <div>Services</div>
+            <div  onClick={()=>navigate('/cms')}>Admin Panel</div>
         </nav>
     </>
   )
